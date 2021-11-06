fd292fa6e7f0212c752df8ed3c54914e0da382ff
mat <github@matdoes.dev>
2021-11-06 18:43:35 -0500

    try using vite-plugin-pwa

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess'
 import staticAdapter from '@sveltejs/adapter-static'
+import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,16 +11,44 @@ const config = {
 	kit: {
 		adapter: staticAdapter(),
 		target: '#svelte',
-		vite: process.env.REPL_ID
-			? {
-					hmr: {
+		vite: {
+			hmr: process.env.REPL_ID
+				? {
 						server: {
 							protocol: 'wss',
 							port: 443,
 						},
+				  }
+				: undefined,
+			plugins: [
+				VitePWA({
+					manifest: {
+						short_name: 'Web Utils',
+						name: 'Web Utils',
+						start_url: '/',
+						icons: [
+							{
+								src: 'favicon-512.png',
+								type: 'image/png',
+								sizes: '512x512',
+							},
+							{
+								src: 'favicon.png',
+								type: 'image/png',
+								sizes: '16x16',
+							},
+						],
+						background_color: '#21222c',
+						display: 'standalone',
+						scope: '/',
+						theme_color: '#21222c',
+						description: 'Random useful things for developers',
 					},
-			  }
-			: undefined,
+					registerType: 'autoUpdate',
+				}),
+			],
+		},
+		ssr: false,
 	},
 }
 
