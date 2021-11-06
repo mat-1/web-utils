0761642cd3c834dd48e199a05260bebff0affc20
mat <github@matdoes.dev>
2021-11-06 18:50:57 -0500

    Revert "try using vite-plugin-pwa"
    This reverts commit fd292fa6e7f0212c752df8ed3c54914e0da382ff.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess'
 import staticAdapter from '@sveltejs/adapter-static'
-import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,44 +10,16 @@ const config = {
 	kit: {
 		adapter: staticAdapter(),
 		target: '#svelte',
-		vite: {
-			hmr: process.env.REPL_ID
-				? {
+		vite: process.env.REPL_ID
+			? {
+					hmr: {
 						server: {
 							protocol: 'wss',
 							port: 443,
 						},
-				  }
-				: undefined,
-			plugins: [
-				VitePWA({
-					manifest: {
-						short_name: 'Web Utils',
-						name: 'Web Utils',
-						start_url: '/',
-						icons: [
-							{
-								src: 'favicon-512.png',
-								type: 'image/png',
-								sizes: '512x512',
-							},
-							{
-								src: 'favicon.png',
-								type: 'image/png',
-								sizes: '16x16',
-							},
-						],
-						background_color: '#21222c',
-						display: 'standalone',
-						scope: '/',
-						theme_color: '#21222c',
-						description: 'Random useful things for developers',
 					},
-					registerType: 'autoUpdate',
-				}),
-			],
-		},
-		ssr: false,
+			  }
+			: undefined,
 	},
 }
 
