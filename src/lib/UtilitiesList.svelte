ea7d2af871225e8562f2c3bad4dcd23a111118e0
mat <github@matdoes.dev>
2021-11-08 19:18:46 -0600

    tweak search bar and list a bit more

--- src/lib/UtilitiesList.svelte
+++ src/lib/UtilitiesList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation'
+
 	import ListItem from '$lib/ListItem.svelte'
 
 	let searchQuery = ''
@@ -33,18 +35,35 @@
 			href: '/lipsum',
 		},
 	]
+
+	$: shownListItems = utilities
+		.filter((u) => {
+			return matchesSearchQuery(searchQuery, u.name)
+		})
+		.sort((a, b) => a.name.length - b.name.length)
+
+	function onSearchKeydown(event: KeyboardEvent) {
+		console.log(event)
+		if (event.key === 'Enter') {
+			if (searchQuery.length > 0 && shownListItems.length > 0 && 'href' in shownListItems[0]) {
+				goto(shownListItems[0].href)
+			}
+		}
+	}
 </script>
 
 <div class="utilities-list-container">
 	<div class="search-container">
-		<input type="text" placeholder="Search" class="search" bind:value={searchQuery} />
+		<input
+			type="text"
+			placeholder="Search"
+			class="search"
+			bind:value={searchQuery}
+			on:keydown={onSearchKeydown}
+		/>
 	</div>
 	<ul>
-		{#each utilities
-			.filter((u) => {
-				return matchesSearchQuery(searchQuery, u.name)
-			})
-			.sort((a, b) => a.name.length - b.name.length) as utility (utility.name)}
+		{#each shownListItems as utility (utility.name)}
 			<ListItem href={utility.href}>{utility.name}</ListItem>
 		{/each}
 	</ul>
@@ -59,20 +78,24 @@
 		list-style: none;
 		padding: 0;
 		margin-top: 0;
+		overflow: scroll;
+		height: calc(100vh - 3rem);
 	}
 	.search {
 		display: block;
 		width: 100%;
+		margin-bottom: 0.5rem;
 	}
 	@media (max-width: 440px) {
 		.search {
 			position: absolute;
-			top: 0.325rem;
+			top: 0.55rem;
 			width: calc(100% - 3rem);
 			left: 2.5rem;
 		}
 		ul {
-			margin-top: -0.85rem;
+			margin-top: -0.7rem;
+			height: calc(100vh - 2.5rem);
 		}
 	}
 	.search-container {
